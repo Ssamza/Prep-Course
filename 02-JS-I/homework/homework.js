3d4e81be043b47c7dab272fe29bdb510aba7b88f
Ssamza <santiagoemezeta@gmail.com>
2022-11-24 21:45:42 -0500

    homework 02-JS-I, DONE

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -197,9 +197,10 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return str  + "!";
+  return str + "!";
 }
-agregarSimboloExclamacion("hellow world");
+agregarSimboloExclamacion("hellow"," world");
+
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
